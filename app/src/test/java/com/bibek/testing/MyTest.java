39fbd53967880c56201e3afba41ce7111359927a
bvekv1 <bvekp1@gmail.com>
2020-01-21 11:44:59 +0545

    Initial commit

---
+++ app/src/test/java/com/bibek/testing/MyTest.java
@@ -0,0 +1,15 @@
+package com.bibek.testing;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MyTest {
+    @Test
+    public void  testAddition(){
+        Arithmetic arithmetic = new Arithmetic();
+        float expectedResult =10;
+        float actualResult = arithmetic.add(4,6);
+        assertEquals(expectedResult,actualResult);
+    }
+}
